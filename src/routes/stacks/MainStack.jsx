b9bfc7957b52fd2455316de12cc9606eb69751f4
husam287 <husam287@hotmai.com>
2022-11-01 17:29:07 +0200

    fix stack version

--- src/routes/stacks/MainStack.jsx
+++ src/routes/stacks/MainStack.jsx
@@ -1,10 +1,10 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import BottomNavigator from 'routes/tab-navigator/BottomTabNavigator';
 import TestScreen from 'screens/TestScreen';
 import RouterOption from '../header-options/RouterOption';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function MainStack() {
   return (
