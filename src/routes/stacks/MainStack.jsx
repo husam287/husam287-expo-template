4cf8514d122fb94e9fd03ee0a7d3c829ad2d8d32
husam287 <husam287@hotmai.com>
2022-11-17 09:19:25 +0200

    fix react stack depen.

--- src/routes/stacks/MainStack.jsx
+++ src/routes/stacks/MainStack.jsx
@@ -1,10 +1,10 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import BottomNavigator from 'routes/tab-navigator/BottomTabNavigator';
 import TestScreen from 'screens/TestScreen';
 import RouterOption from '../header-options/RouterOption';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function MainStack() {
   return (
