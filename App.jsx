ce5f98e2329c47c437bd1d3547c0d8b9750bbc6f
husam287 <husam287@hotmai.com>
2023-01-10 20:25:59 +0200

    add splash screen manuall hide feature

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import useCachedResources from 'hooks/useCachedResurces';
 import Route from 'routes';
 import store from 'reducers';
+import useCachedResources from 'hooks/useCachedResources';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
--- src/hooks/useCachedResources.js
+++ src/hooks/useCachedResources.js
@@ -6,6 +6,7 @@ import font400 from 'assets/fonts/Changa-Regular.ttf';
 import font500 from 'assets/fonts/Changa-Medium.ttf';
 import font600 from 'assets/fonts/Changa-SemiBold.ttf';
 import font700 from 'assets/fonts/Changa-Bold.ttf';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function useCachedResources() {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -14,6 +15,8 @@ export default function useCachedResources() {
   useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
+        SplashScreen.preventAutoHideAsync();
+
         // Load fonts
         await Font.loadAsync({
           IcoMoon,
@@ -26,6 +29,7 @@ export default function useCachedResources() {
         // We might want to provide this error information to an error reporting service
       } finally {
         setLoadingComplete(true);
+        SplashScreen.hideAsync();
       }
     }
 
